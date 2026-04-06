4a9d47a14e00981d81e66d01cc03e186732f1542
Stubberson <tuukka.j.korpela@gmail.com>
2026-04-06 14:41:30 +0200

    Start building Task class

--- src/tasks/create-task.js
+++ src/tasks/create-task.js
@@ -3,11 +3,14 @@ function createTask(projectContainer) {
     const taskCompleteCheckmark = document.createElement('input')
     const taskDescription = document.createElement('input')
 
+    taskContainer.className = 'task-container'
+
     taskCompleteCheckmark.className = 'task-complete'
     taskCompleteCheckmark.type = 'checkbox'
 
     taskDescription.className = 'task-description'
     taskDescription.type = 'text'
+    taskDescription.name = 'task-description'
     taskDescription.placeholder = 'Add task...'
 
     taskContainer.append(taskCompleteCheckmark, taskDescription)

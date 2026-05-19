c1c7a88f97c2bbd957d6ac103b4bb05e83436898
Stubberson <tuukka.j.korpela@gmail.com>
2026-05-19 16:17:47 +0200

    Time picker nice visually, no functionality

--- src/utilities/time.js
+++ src/utilities/time.js
@@ -9,21 +9,25 @@ function createTimePicker() {
 
     const hourContainer = document.createElement('div')
     hourContainer.className = 'time-picker-hour'
+    hourContainer.textContent = '[hh]'
 
     const minuteContainer = document.createElement('div')
     minuteContainer.className = 'time-picker-minute'
+    minuteContainer.textContent = '[mm]'
     
+    // Hours
     for (let i = 0; i < 24; i++) {
         const hour = document.createElement('span')
         i < 10 ? hour.textContent = `0${i}` : hour.textContent = i
-        i === currentTime.hour ? hour.hidden = false : hour.hidden = true
+        hour.hidden = true
         hourContainer.append(hour)
     }
 
+    // Minutes
     for (let i = 0; i < 60; i++) {
         const minute = document.createElement('span')
         i < 10 ? minute.textContent = `0${i}` : minute.textContent = i
-        i === currentTime.minute ? minute.hidden = false : minute.hidden = true
+        minute.hidden = true
         minuteContainer.append(minute)
     }
 
